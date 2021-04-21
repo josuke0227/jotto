e144407a8ef56852f0d48d31ba9dfbb64503a598
Josuke <y.motosugi0227@gmail.com>
2021-04-21 12:56:44 +1000

    Set up Redux thunk

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
-export default createStore(rootReducer);
+export const middlewares = [ReduxThunk];
+
+export default createStore(rootReducer, {}, applyMiddleware(...middlewares));
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,16 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../src/reducers/index";
+import { middlewares } from "../src/configureStore";
 
-export const storeFactory = (initialState) =>
-  createStore(rootReducer, initialState);
+export const storeFactory = (initialState) => {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
+};
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
