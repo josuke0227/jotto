40efb252a078cf807d9d948185155da0d90fad6d
Josuke <y.motosugi0227@gmail.com>
2021-04-21 15:12:19 +1000

    set up getSecretword reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { getLetterMatchCount } from "../helpers/index";
 import axios from "axios";
 
 export const actionTypes = {
@@ -6,7 +7,22 @@ export const actionTypes = {
 };
 
 export const guessWord = (guessedWord) => {
-  return function (dispatch, getState) {};
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: {
+        guessedWord,
+        letterMatchCount,
+      },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORREECT_GUESS });
+    }
+  };
 };
 
 export const getSecretWord = () => {
