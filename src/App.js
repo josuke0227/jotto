f6f2353a7c89a59099a92a9d2cba552ded811563
Josuke <y.motosugi0227@gmail.com>
2021-04-22 14:59:39 +1000

    Hide secretword

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
+      <div>The secret word is {secretWord}</div>
       <Congrats success={success} />
       <Input success={success} secretWord={secretWord} />
       <GuessedWords guessedWords={guessedWords} />
